6df1f22acf7faba034e37426bd5b003ced0ba1b4
Armin joyaian <137637877+Arminjoyaian@users.noreply.github.com>
2023-07-13 12:06:53 +0330

    Add files via upload

---
+++ number_game2.py
@@ -0,0 +1,18 @@
+import random
+a = 0
+b =100
+number = int(input("Enter number : "))
+number_2 = random.randint(a ,b)
+while True:
+    if number_2 == number :
+        print(f"computer:{number_2}","you win")
+        player += 1
+        break
+    elif number_2 > number:
+        print(f"computer:{number_2}", "go down")
+        number_2 == b 
+        number_2 = random.randint(a ,b)
+    else:
+        print(f"computer:{number_2}", "go up")
+        player = number_2
+        number_2 = random.randint(a ,b )
